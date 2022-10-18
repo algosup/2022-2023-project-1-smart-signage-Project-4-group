667dd8771cabb04d6f4dbfddbd303fdc00687c90
florenthureaux <florent.hureaux@algosup.com>
2022-10-18 15:55:32 +0200

    correct some mistake

--- Tests/sensor_test.go
+++ Tests/sensor_test.go
@@ -26,16 +26,16 @@ func TestGetCurrentSensorData(t *testing.T) {
 func TestReduceLightIntensity(t *testing.T) {
 	cases := []struct {
 		name     string
-		isreduce int
+		isreduce bool
 		want     bool
 	}{
-		{"Change intensity to 50%", true, true,
+		{"Change intensity to 50%", true, true},
 		{"Change intensity to 20%", false, true},
 	}
 
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			got := Light(c.isreduce)
+			got := Light(c.isreduce, true)
 			if got != c.want {
 				t.Fatalf("got %t, want %t", got, c.want)
 			}
