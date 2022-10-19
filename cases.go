8f872b84147080695bab21f4385f656d80228f83
laurentbouquin <laurent.bouquin@algosup.com>
2022-10-19 14:56:52 +0200

    Update cases.go
    Change the and to or logic gate in the condition

--- cases.go
+++ cases.go
@@ -6,7 +6,7 @@ import (
 
 func TimeRules() {
 	for {
-		if time.Now().Hour() >= 6 && time.Now().Hour() <= 1 {
+		if time.Now().Hour() >= 6 || time.Now().Hour() <= 1 {
 			Light(false, true)
 		} else {
 			Light(false, false)
