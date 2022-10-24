9cbec9e5100ccdac5d2bc0f6a078178dc67134db
florenthureaux <florent.hureaux@algosup.com>
2022-10-24 09:26:59 +0200

    fix sensor.go
    add comment

--- Sensor.go
+++ Sensor.go
@@ -14,6 +14,7 @@ func GetCurrentSensorData() (bool, bool) { // Function that get the state of the
 	out2 := (sensor2.Get())
 	return out1, out2
 }
+
 /*
 func Light(isReduce bool, isOn bool) { // Function used to switch the light ON/OFF and control the intensity
 	rate := time.Second / 100000000
@@ -37,48 +38,48 @@ func Light(isReduce bool, isOn bool) { // Function used to switch the light ON/O
 */
 
 type LED struct {
-    pin machine.Pin
-    on  bool
+	pin machine.Pin
+	on  bool
 }
 
 func New() LED {
-    leds := machine.PC13
-    leds.Configure(machine.PinConfig{
-        Mode: machine.PinOutput,
-    })
-    l := LED{
-        pin: leds,
-        on:  false,
-    }
-    return &l
+	leds := machine.PC13
+	leds.Configure(machine.PinConfig{
+		Mode: machine.PinOutput,
+	})
+	l := LED{
+		pin: leds,
+		on:  false,
+	}
+	return &l
 }
 
 // On indicates if the LED is turned on.
-func (lLED) On() bool {
-    return l.on
+func (l LED) On() bool {
+	return l.on
 }
 
 // Set sets the LED to the value.
 // true will turn the LED on
 // false will turn the LED off
 func (l LED) Set(value bool) {
-    l.on = value
-    if l.on {
-        l.pin.High()
-        return
-    } else {
-        l.pin.Low()
-    }
+	l.on = value
+	if l.on {
+		l.pin.High()
+		return
+	} else {
+		l.pin.Low()
+	}
 }
 
-func (lLED) Blink() {
-    rate := time.Second / 500
-    for i := 0; i < 1000; i++ {
-        l.High()
-        l.Set(true)
-        time.Sleep(rate)
-        l.Low()
-        l.Set(false)
-        time.Sleep(rate)
-    }
+func (l LED) Blink() { // Function used to blink the LED
+	rate := time.Second / 500
+	for i := 0; i < 1000; i++ {
+		l.High()
+		l.Set(true)
+		time.Sleep(rate)
+		l.Low()
+		l.Set(false)
+		time.Sleep(rate)
+	}
 }
