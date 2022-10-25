01abee5bee79a9476d95f5efa17ea76a33cd6366
Robin Debry <91249812+robin-debry@users.noreply.github.com>
2022-10-25 11:46:32 +0200

    update main.go
    try to improve to send AT command

--- main.go
+++ main.go
@@ -6,21 +6,41 @@ import (
 )
 
 func main() {
-	CustomerOn := true
 	machine.UART1.Configure(machine.UARTConfig{BaudRate: 9600, TX: machine.PA2, RX: machine.PA3})
 	_, join := machine.UART1.Write([]byte("AT+JOIN\r\n"))
-	_, msg := machine.UART1.Write([]byte("AT+MSG='allumé'\r\n"))
-
-	if CustomerOn {
-
+	rate2 := time.Second * 10
+	time.Sleep(rate2)
+	//_, read := machine.UART1.Read(join)
+	if join != nil {
+		time.Sleep(rate2)
+		machine.UART1.Write([]byte("AT+MSG='Joined'\r\n"))
 		println(join)
+	}
+
+	for i := 0; i < 100; i++ {
+		zqsedrftgy(false)
+	}
+	for i := 0; i < 100; i++ {
+		zqsedrftgy(true)
+	}
+
+}
+func zqsedrftgy(CustomerOn bool) {
+	if CustomerOn {
+		//_, join := machine.UART1.Write([]byte("AT+JOIN\r\n"))
+
+		//println(join)
 		Light(true, true)
 
-		println(msg)
+		//_, msg := machine.UART1.Write([]byte("AT+MSG='allumé'\r\n"))
+		//_, read := machine.UART1.Read(msg)
+		//println(msg)
+		//println(read)
 	} else {
 		Light(false, false)
 
 	}
+
 }
 
 func Light(isReduce bool, isOn bool) {
